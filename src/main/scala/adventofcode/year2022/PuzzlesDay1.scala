aa99cd6c39d2e125ff7405b4886ca9a52f1c75f6
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2022-12-10 18:51:49 +0100

    Add 2022.1 puzzles

---
+++ src/main/scala/adventofcode/year2022/PuzzlesDay1.scala
@@ -0,0 +1,41 @@
+package adventofcode.year2022
+
+import scala.io.Source
+
+object PuzzlesDay1 extends App {
+
+  case class Elf(calories: List[Int])
+
+  def puzzle(
+    input: String
+  ) = {
+    read(input)
+      .max
+  }
+
+  def puzzleTwo(
+    input: String
+  ) = {
+    read(input)
+      .sorted
+      .reverse
+      .take(3)
+      .sum
+  }
+
+  def read(
+    input: String
+  ) = {
+    case class State(ls: List[Int] = Nil, calory: Int = 0)
+
+    val state = Source.fromFile(input)
+      .getLines
+      .foldLeft(State()) {
+        (state, line) => line match {
+          case "" => State(state.calory :: state.ls, 0)
+          case s"$calory" => State(state.ls, state.calory + calory.toInt)
+        }
+      }
+    state.calory :: state.ls
+  }
+}
